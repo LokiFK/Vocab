c3bcc5b84cb113aa477f43462b62a8ad5ba4b68b
LokiFK <60155802+LokiFK@users.noreply.github.com>
2020-11-21 11:56:27 +0100

    Comment VocabBox

--- src/vocabulary/Node.java
+++ src/vocabulary/Node.java
@@ -5,22 +5,27 @@ public class Node<T> {
     private Node<T> nextNode;
     private final T entry;
 
-    public Node(T entry) {
-        this.entry = entry;
-    }
+//    sets the entry so that the node is never null
+public Node(T entry) {
+    this.entry = entry;
+}
 
+    //    gets the next node
     public Node<T> getNextNode() {
         return nextNode;
     }
 
+    //    sets the next node
     public void setNextNode(Node<T> nextNode) {
         this.nextNode = nextNode;
     }
 
+    //    gets the entry of current node
     public T getEntry() {
         return entry;
     }
 
+    //    returns entry.toString()
     public String toString() {
         return entry.toString();
     }
--- src/vocabulary/VocabBox.java
+++ src/vocabulary/VocabBox.java
@@ -5,30 +5,37 @@ public class VocabBox {
   private Node<Vocab> startNode;
 
   public boolean addVocab(Vocab v) {
+//    CASE 1: list is empty
     if (startNode == null) {
       startNode = new Node<>(v);
       return true;
     }
+//    CASE 2: new vocab is supposed to be added to the second node
     if (startNode.getEntry().getDeutsch().compareTo(v.getDeutsch()) > 0) {
       Node<Vocab> node = new Node<>(v);
       node.setNextNode(startNode);
       startNode = node;
       return true;
     }
+//    CASE 3: new vocab is supposed to be added in middle or end of list
     Node<Vocab> cursor = startNode;
     while (cursor.getNextNode() != null) {
       if (cursor.getNextNode().getEntry().getDeutsch().compareTo(v.getDeutsch()) < 0) {
         cursor = cursor.getNextNode();
       } else break;
     }
+//    swaps current node with new node
     Node<Vocab> node = new Node<>(v);
     node.setNextNode(cursor.getNextNode());
     cursor.setNextNode(node);
     return true;
   }
 
+  //  Gets size of the list
   public int size() {
+//    CASE 1: list is empty
     int count = 0;
+//    CASE 2: list is not empty
     if (startNode != null) count = 1;
     Node<Vocab> cursor = startNode;
     while (cursor != null) {
@@ -38,8 +45,10 @@ public class VocabBox {
     return count;
   }
 
+  //  gets the vocab with index nr
   public Vocab getVocab(int nr) {
     Node<Vocab> cursor = startNode;
+//    iterates through the list until nr reached
     while (nr > 0) {
       if (cursor.getNextNode() != null) {
         cursor = cursor.getNextNode();
@@ -50,11 +59,14 @@ public class VocabBox {
     return cursor.getEntry();
   }
 
-  public boolean deleteVocab(String name) {
-    return true;
-  }
-
+  //  deletes vocab with index nr
   public void deleteVocab(int nr) {
+//    CASE 1: node to delete is in the beginning of the list
+    if (nr == 0) {
+      startNode = startNode.getNextNode();
+      return;
+    }
+//    CASE 2: node to delete is in the middle or at the end of the list
     Node<Vocab> cursor = startNode;
     while (nr > 1) {
       if (cursor.getNextNode() != null) {
@@ -62,14 +74,20 @@ public class VocabBox {
       } else return;
       nr--;
     }
-    cursor.setNextNode(cursor.getNextNode().getNextNode());
+    if (cursor.getNextNode() != null)
+      cursor.setNextNode(cursor.getNextNode().getNextNode());
+    else
+//      CASE 3: node to delete is at the end of the list
+      startNode = null;
   }
 
+  //  updates vocab with index nr
   public void updateVocab(Vocab vocab, int nr) {
     deleteVocab(nr);
     addVocab(vocab);
   }
 
+  //  returns the list as string
   @Override
   public String toString() {
     StringBuilder stringBuilder = new StringBuilder();
